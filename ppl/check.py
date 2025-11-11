098a6dd5072ea01f1aa015fad1f2c52807264d87
divyaansh <parmardivyaansh8@gmail.com>
2025-11-11 21:40:22 +0530

    final commit

--- ppl/check.py
+++
@@ -1,7 +0,0 @@
-ch= input("enter a character: ")[0]
-if('0'<=ch<='9'):
-    print("it is an integer")
-elif('a'<=ch<='z'):
-    print("it is a lowercase")
-elif('A'<=ch<='Z'):
-    print("it is an uppercase")
--- ppl/factorial.py
+++
@@ -1,8 +0,0 @@
-def factorial(number):
-    if number == 0 or number == 1:
-        return 1
-    else:
-        return number * factorial(number - 1)
-
-n = int(input("Enter the number: "))
-print("The factorial of the entered number is:", factorial(n))
